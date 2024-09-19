08d426bd94453ab694f098e9e67c523ae9ddffdd
Levon009 <levonhakobyan1989@gmail.com>
2024-09-19 22:20:25 +0400

    Expendable list

--- app/src/main/java/com/example/jetpack_multiplenavigation/notes/presentation/notes/components/NoteItem.kt
+++ app/src/main/java/com/example/jetpack_multiplenavigation/notes/presentation/notes/components/NoteItem.kt
@@ -1,5 +1,8 @@
 package com.example.jetpack_multiplenavigation.notes.presentation.notes.components
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,13 +10,23 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
@@ -26,6 +39,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.ColorUtils
 import com.example.jetpack_multiplenavigation.notes.domain.model.Note
@@ -38,6 +52,17 @@ fun NoteItem(
     cutCornerRadius: Dp = 30.dp,
     onDeleteClick: () -> Unit
 ) {
+    var isExpended by remember {
+        mutableStateOf(false)
+    }
+    val expendedItemSizeState by animateDpAsState(
+        targetValue = if (isExpended) 30.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ),
+        label = "Expend list"
+    )
     Box(modifier = modifier) {
         Canvas(modifier = Modifier.matchParentSize()) {
             val clipPath = Path().apply {
@@ -81,17 +106,23 @@ fun NoteItem(
                 maxLines = 1
             )
             Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = note.content,
-                color = MaterialTheme.colorScheme.onSurface,
-                style = MaterialTheme.typography.bodyLarge,
-                fontFamily = FontFamily.Serif,
-                maxLines = 10
-            )
+            if (isExpended) {
+                NoteExpendableContent(
+                    note = note,
+                    modifier = Modifier.padding(
+                        start = 8.dp,
+                        top = expendedItemSizeState.coerceAtLeast(0.dp),
+                        end = 8.dp,
+                        bottom = expendedItemSizeState.coerceAtLeast(0.dp)
+                    )
+                )
+            }
         }
         IconButton(
             onClick = onDeleteClick,
-            modifier = Modifier.align(Alignment.BottomEnd)
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(end = 32.dp)
         ) {
             Icon(
                 imageVector = Icons.Default.Delete,
@@ -99,5 +130,31 @@ fun NoteItem(
                 tint = MaterialTheme.colorScheme.onSurface
             )
         }
+        IconButton(
+            onClick = { isExpended = !isExpended },
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            Icon(
+                imageVector = if (isExpended) Icons.Default.KeyboardArrowUp
+                else Icons.Default.KeyboardArrowDown,
+                contentDescription = "Open content"
+            )
+        }
+    }
+}
+
+@Composable
+private fun NoteExpendableContent(
+    modifier: Modifier = Modifier,
+    note: Note
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = note.content,
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.bodyLarge,
+            fontFamily = FontFamily.Serif,
+            maxLines = 10
+        )
     }
 }
