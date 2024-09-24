d4acfd31e7927cfc20984e12f6472cd7809a440b
Levon009 <levonhakobyan1989@gmail.com>
2024-09-24 17:26:59 +0400

    Api

--- app/src/main/java/com/example/jetpack_multiplenavigation/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_multiplenavigation/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -57,6 +58,7 @@ class MainActivity : ComponentActivity() {
     private val imageViewModel by viewModels<ImageViewModel>()
     private val tag = "Kotlin_Flows"
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         hideStatusBar()
