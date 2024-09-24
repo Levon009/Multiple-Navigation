8873e8efc55f9e4d89d9495b9c3c24ab860d02a3
Levon009 <levonhakobyan1989@gmail.com>
2024-09-24 17:26:34 +0400

    D

--- app/src/main/java/com/example/jetpack_multiplenavigation/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_multiplenavigation/MainActivity.kt
@@ -9,7 +9,6 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
