96d591e1d93e0e080655a5865d4d96278a7cfc71
Levon009 <levonhakobyan1989@gmail.com>
2024-11-30 21:40:51 +0400

    data object

--- app/src/main/java/com/example/jetpack_multiplenavigation/notes/presentation/add_edit_note/UIEvents.kt
+++ app/src/main/java/com/example/jetpack_multiplenavigation/notes/presentation/add_edit_note/UIEvents.kt
@@ -2,5 +2,5 @@ package com.example.jetpack_multiplenavigation.notes.presentation.add_edit_note
 
 sealed class UIEvents {
     data class ShowSnackBar(val message: String) : UIEvents()
-    object SaveNote : UIEvents()
+    data object SaveNote : UIEvents()
 }
